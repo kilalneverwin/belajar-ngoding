defffe012093119bf7b4e122b9bd809e74f503f2
kilalneverwin <amirkilal1606@gmail.com>
2025-07-16 19:45:12 +0700

    Add files via upload

---
+++ module/main_alias.py
@@ -0,0 +1,15 @@
+# module matematika dengan import
+
+from matematika import tambah as add
+from matematika import kali as k
+from matematika import pangkat as p
+# import matematika as math
+
+hasil_tambah = add(1,2,3,4,5)
+print(f"hasil tambah = {hasil_tambah}")
+
+hasil_kali = k(1,2,3,4,5)
+print(f"hasil tambah = {hasil_kali}")
+
+pangkat3 = p(3)
+print(f"hasil pangkat3 = {pangkat3(3)}")
